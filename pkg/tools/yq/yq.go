5ba152b2b9878bc1d00b1598dcc5b3f5f6854d06
Trevor Nierman <tnierman@redhat.com>
2024-03-21 16:26:09 -0500

    Fix yq insallation

--- pkg/tools/yq/yq.go
+++ pkg/tools/yq/yq.go
@@ -35,13 +35,16 @@ func (t *Tool) Install() error {
 		return err
 	}
 
-	matches := github.FindAssetsForArchAndOS(release.Assets)
+	matches := github.FindAssetsExcluding([]string{".tar.gz"}, github.FindAssetsForArchAndOS(release.Assets))
 	if len(matches) != 1 {
 		return fmt.Errorf("unexpected number of assets found matching system spec: expected 1, got %d.\nMatching assets: %v", len(matches), matches)
 	}
-	toolArchiveAsset := matches[0]
+	toolAsset := matches[0]
 
-	matches = github.FindAssetsContaining([]string{"checksums.txt"}, release.Assets)
+	matches, err = github.FindAssetsMatching("^checksums$", release.Assets)
+	if err != nil {
+		return fmt.Errorf("failed to filter assets by regular expression: %w", err)
+	}
 	if len(matches) != 1 {
 		return fmt.Errorf("unexpected number of checksum assets found: expected 1, got %d.\nMatching assets: %v", len(matches), matches)
 	}
@@ -55,51 +58,41 @@ func (t *Tool) Install() error {
 		return fmt.Errorf("failed to create version-specific directory '%s': %w", versionedDir, err)
 	}
 
-	err = t.Source.DownloadReleaseAssets([]*gogithub.ReleaseAsset{checksumAsset, toolArchiveAsset}, versionedDir)
+	err = t.Source.DownloadReleaseAssets([]*gogithub.ReleaseAsset{checksumAsset, toolAsset}, versionedDir)
 	if err != nil {
 		return fmt.Errorf("failed to download one or more assets: %w", err)
 	}
 
 	// Verify checksum of downloaded assets
-	toolArchiveFilepath := filepath.Join(versionedDir, toolArchiveAsset.GetName())
+	toolArchiveFilepath := filepath.Join(versionedDir, toolAsset.GetName())
 	binarySum, err := utils.Sha256sum(toolArchiveFilepath)
 	if err != nil {
 		return fmt.Errorf("failed to calculate checksum for '%s': %w", toolArchiveFilepath, err)
 	}
 
 	checksumFilePath := filepath.Join(versionedDir, checksumAsset.GetName())
-	checksumLine, err := utils.GetLineInFileMatchingKey(checksumFilePath, toolArchiveAsset.GetName())
+	checksumLine, err := utils.GetLineInFileMatchingKey(checksumFilePath, toolAsset.GetName())
 	if err != nil {
 		return fmt.Errorf("failed to retrieve checksum from file '%s': %w", checksumFilePath, err)
 	}
-	checksumTokens := strings.Fields(checksumLine)
-	if len(checksumTokens) != 2 {
-		return fmt.Errorf("the checksum file '%s' is invalid: expected 2 fields, got %d", checksumFilePath, len(checksumTokens))
-	}
-	actual := checksumTokens[0]
 
-	toolExecutable := t.ExecutableName()
-	if strings.TrimSpace(binarySum) != strings.TrimSpace(actual) {
-		return fmt.Errorf("warning: Checksum for '%s' does not match the calculated value. Please retry installation. If issue persists, this tool can be downloaded manually at %s", toolExecutable, toolArchiveAsset.GetBrowserDownloadURL())
-	}
-
-	// Untar binary bundle
-	err = utils.Unarchive(toolArchiveFilepath, versionedDir)
-	if err != nil {
-		return fmt.Errorf("failed to unarchive the '%s' asset file '%s': %w", toolExecutable, toolArchiveFilepath, err)
+	// For some reason, yq ships several checksum formats for each asset in its 'checksums' file.
+	// Its honestly less fragile to check if _any_ of the columns contain our calculated checksum than try to decipher which column corresponds to which format
+	if !strings.Contains(checksumLine, strings.TrimSpace(binarySum)) {
+		return fmt.Errorf("warning: Checksum for yq does not match the calculated value. Please retry installation. If issue persists, this tool can be downloaded manually at %s", toolAsset.GetBrowserDownloadURL())
 	}
 
 	// Link as latest
 	latestFilePath := t.SymlinkPath()
 	err = os.Remove(latestFilePath)
 	if err != nil && !os.IsNotExist(err) {
-		return fmt.Errorf("failed to remove existing '%s' binary at '%s': %w", toolExecutable, base.LatestDir, err)
+		return fmt.Errorf("failed to remove existing '%s' binary at '%s': %w", toolAsset.GetName(), base.LatestDir, err)
 	}
 
-	toolBinaryFilepath := filepath.Join(versionedDir, toolExecutable)
+	toolBinaryFilepath := filepath.Join(versionedDir, toolAsset.GetName())
 	err = os.Symlink(toolBinaryFilepath, latestFilePath)
 	if err != nil {
-		return fmt.Errorf("failed to link new '%s' binary to '%s': %w", toolExecutable, base.LatestDir, err)
+		return fmt.Errorf("failed to link new '%s' binary to '%s': %w", toolAsset.GetName(), base.LatestDir, err)
 	}
 	return nil
 }
