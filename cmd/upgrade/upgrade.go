de675b7f2efff7e579f1f423e066a0b3bbea8072
Manuel Dewald <mdewald@redhat.com>
2024-05-02 12:14:06 +0200

    Upgrade backplane-tools first
    * backplane-tools upgrade all upgrades backplane-tools first
    * After upgrading, it reruns the same command to upgrade all other tools
    
    Jira: OSD-22857

--- cmd/upgrade/upgrade.go
+++ cmd/upgrade/upgrade.go
@@ -2,9 +2,12 @@ package upgrade
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/openshift/backplane-tools/pkg/tools"
+	"github.com/openshift/backplane-tools/pkg/tools/self"
 	"github.com/openshift/backplane-tools/pkg/utils"
 	"github.com/spf13/cobra"
 )
@@ -27,29 +30,54 @@ func Cmd() *cobra.Command {
 
 // Upgrade upgrades the provided tools to their latest versions
 func Upgrade(args []string) error {
-	var listTools []tools.Tool
 	if len(args) == 0 || utils.Contains(args, "all") {
 		// If user explicitly passes 'all' or doesn't specify which tools to install,
-		// upgrade everything that's been installed locally
-		var err error
-		listTools, err = tools.ListInstalled()
+		// upgrade backplane-tools itself, then everything that's been installed locally
+		err := upgradeAll()
 		if err != nil {
 			return err
 		}
-	} else {
-		// otherwise build the list verifying tool exist
-		toolMap := tools.GetMap()
+		return nil
+	}
+	// otherwise build the list verifying tool exist
+	toolMap := tools.GetMap()
 
-		listTools = []tools.Tool{}
-		for _, toolName := range args {
-			t, found := toolMap[toolName]
-			if !found {
-				return fmt.Errorf("failed to locate '%s' in list of supported tools", toolName)
-			}
-			listTools = append(listTools, t)
+	listTools := []tools.Tool{}
+	for _, toolName := range args {
+		t, found := toolMap[toolName]
+		if !found {
+			return fmt.Errorf("failed to locate '%s' in list of supported tools", toolName)
 		}
+		listTools = append(listTools, t)
+	}
+	err := upgradeTools(listTools)
+	if err != nil {
+		return err
 	}
 
+	return nil
+}
+
+func upgradeAll() error {
+	upgraded, err := upgradeSelfIfNecessary()
+	if err != nil {
+		return err
+	}
+	if upgraded {
+		return rerunUpgradeAll()
+	}
+	listTools, err := tools.ListInstalled()
+	if err != nil {
+		return err
+	}
+	err = upgradeTools(listTools)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func upgradeTools(listTools []tools.Tool) error {
 	fmt.Println("Upgrading the following tools: ")
 	upgradeList := []tools.Tool{}
 	for _, t := range listTools {
@@ -72,6 +100,42 @@ func Upgrade(args []string) error {
 	err := tools.Install(upgradeList)
 	if err != nil {
 		return fmt.Errorf("failed to upgrade tools: %w", err)
+	}
+	return nil
+}
+
+func upgradeSelfIfNecessary() (bool, error) {
+	selfTool := self.New()
+	latestVersion, err := selfTool.LatestVersion()
+	if err != nil {
+		return false, fmt.Errorf("failed to determine version for '%s': %w", selfTool.Name(), err)
+	}
+	installedVersion, err := selfTool.InstalledVersion()
+	if err != nil {
+		return false, fmt.Errorf("failed to determine version for '%s': %w", selfTool.Name(), err)
+	}
+
+	if installedVersion == latestVersion {
+		return false, nil
+	}
+	fmt.Printf("upgrading %s %s -> %s\n", selfTool.Name(), installedVersion, latestVersion)
+	err = tools.Install([]tools.Tool{selfTool})
+
+	if err != nil {
+		return false, fmt.Errorf("failed to upgrade backplane-tools: %w", err)
+	}
+	return true, nil
+}
+
+func rerunUpgradeAll() error {
+	fmt.Printf("re-running '%s' to upgrade all tools...\n", strings.Join(os.Args, " "))
+	cmd := exec.Command(os.Args[0], os.Args[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to run '%s': %w", strings.Join(os.Args, " "), err)
 	}
 	return nil
 }
